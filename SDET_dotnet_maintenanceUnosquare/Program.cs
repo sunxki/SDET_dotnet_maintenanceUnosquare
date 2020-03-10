6d3bfa3406f90ea609ad4bab3de9d1df2f7e6d56
Luis Osuna <luis.osuna@unosquare.com>
2020-03-10 17:24:47 -0600

    SDET Maintenance

--- SDET_dotnet_maintenanceUnosquare/Program.cs
+++ SDET_dotnet_maintenanceUnosquare/Program.cs
@@ -1,15 +1,81 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SDET_dotnet_maintenanceUnosquare
+namespace UnoSquare_Maintenance
 {
     class Program
     {
+        IWebDriver driver;
+        public IWebDriver SetUpDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            return driver;
+        }
+
+        public void Click(IWebElement element)
+        {
+            element.Click();
+        }
+
+        public void SendText(IWebElement element, string value)
+        {
+            element.SendKeys(value);
+        }
+
+        #region Google Locators
+        By GoogleSearchBar = By.XPath("need maintenance");
+        By GoogleSearIcon = By.XPath("need maintenance");
+        By UnoSquareGoogleResult = By.XPath("need maintenance");
+        #endregion
+
+        #region UnoSquare Locators
+        By UnoSquareServicesMenu = By.XPath("need maintenance");
+        By PracticeAreas = By.XPath("need maintenance");
+        By ContactUs = By.XPath("need maintenance");
+        #endregion 
         static void Main(string[] args)
         {
+            IWebDriver Browser;
+            IWebElement element;
+            Program program = new Program();
+            Browser = program.SetUpDriver();
+            Browser.Url = "https://www.google.com";
+
+            element = Browser.FindElement(program.GoogleSearchBar);
+
+            program.SendText(element, "Unosquare");
+
+            element = Browser.FindElement(program.GoogleSearIcon);
+
+            program.Click(element);
+
+            element = Browser.FindElement(program.UnoSquareGoogleResult);
+
+            program.Click(element);
+
+            element = Browser.FindElement(program.UnoSquareServicesMenu);
+
+            program.Click(element);
+
+            element = Browser.FindElement(program.PracticeAreas);
+
+            program.Click(element);
+
+            element = Browser.FindElement(program.ContactUs);
+
+            program.Click(element);
+
+
+
+
+
         }
     }
 }
