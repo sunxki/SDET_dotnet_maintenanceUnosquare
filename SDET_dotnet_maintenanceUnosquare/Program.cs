6c29534c889533932fe4a33cc8a20ce31b4e79d2
Luis Osuna <luis.osuna@unosquare.com>
2020-03-10 17:23:09 -0600

    Add project files.

---
+++ SDET_dotnet_maintenanceUnosquare/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDET_dotnet_maintenanceUnosquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
